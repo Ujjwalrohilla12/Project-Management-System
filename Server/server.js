891bd91f7d5dfc16b2e259056261603c89010977
AlishaPraveen070 <alishapraveen2010@gmail.com>
2026-03-09 14:09:25 +0530

    remove inngest completely

--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,6 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
-import { serve } from "inngest/express";
-import { inngest, functions } from "./dist/inngest/index.js"
 import { handleClerkWebhook } from "./dist/routes/clerk-webhook.js"
 
 const app = express();
@@ -14,7 +12,6 @@ app.use(clerkMiddleware());
 
 app.get('/',(req,res)=> res.send('Server is live!'));
 app.post('/api/clerk-webhook', handleClerkWebhook);
-app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000
 
