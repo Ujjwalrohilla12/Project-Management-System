9bab94f3a5cf3c5740861690993bd8f2cacaec10
AlishaPraveen070 <alishapraveen2010@gmail.com>
2026-03-09 11:20:29 +0530

    fix vercel deployment

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.ts"
+import { inngest, functions } from "./inngest/index.js"
 const app = express();
 
 app.use(express.json());
@@ -15,4 +15,8 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
