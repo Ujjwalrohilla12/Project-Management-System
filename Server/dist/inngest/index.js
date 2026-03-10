289050739de22fa27adae829a6ea2386cc4bbb73
AlishaPraveen070 <alishapraveen2010@gmail.com>
2026-03-10 11:18:35 +0530

    add workspace models

--- Server/dist/inngest/index.js
+++ Server/dist/inngest/index.js
@@ -1,13 +1,35 @@
 import { Inngest } from "inngest";
 import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "./user-sync.js";
+import { prisma } from "../configs/prisma.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "aegisflow" });
 // Inngest functions
 const syncUserCreationFn = inngest.createFunction({ id: 'sync-user-from-clerk' }, { event: 'clerk/user.created' }, async ({ event }) => await syncUserCreation(event.data));
 const syncUserDeletionFn = inngest.createFunction({ id: 'delete-user-from-clerk' }, { event: 'clerk/user.deleted' }, async ({ event }) => await syncUserDeletion(event.data));
 const syncUserUpdationFn = inngest.createFunction({ id: 'update-user-from-clerk' }, { event: 'clerk/user.updated' }, async ({ event }) => await syncUserUpdation(event.data));
+const syncWorkspaceCreation = inngest.createFunction({ id: 'sync-workspace-from-clerk' }, { event: 'clerk/organisation.created' }, async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.create({
+        data: {
+            id: data.id,
+            name: data.name,
+            slug: data.slug,
+            ownerId: data.created_by,
+            image_url: data.image_url,
+        }
+    });
+    // add creater as ADMIN Member
+    await prisma.workspaceMember.create({
+        data: {
+            userId: data.created_by,
+            workspaceId: data.id,
+            role: 'ADMIN'
+        }
+    });
+});
 export const functions = [
     syncUserCreationFn,
     syncUserDeletionFn,
-    syncUserUpdationFn
+    syncUserUpdationFn,
+    syncWorkspaceCreation
 ];
--- Server/inngest/index.ts
+++ Server/inngest/index.ts
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "./user-sync.js";
+import { prisma } from "../configs/prisma.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "aegisflow" });
@@ -23,8 +24,37 @@ const syncUserUpdationFn = inngest.createFunction(
     async({event}) => await syncUserUpdation(event.data)
 )
 
+const syncWorkspaceCreation = inngest.createFunction(
+    {id:'sync-workspace-from-clerk'},
+{event: 'clerk/organisation.created'},
+async({event}) =>{
+    const {data} = event;
+    await prisma.workspace.create({
+        data:{
+            id: data.id,
+            name: data.name,
+            slug: data.slug,
+            ownerId: data.created_by,
+            image_url: data.image_url,
+
+        }
+    }
+    )
+    // add creater as ADMIN Member
+    await prisma.workspaceMember.create({
+        data: {
+            userId: data.created_by,
+            workspaceId: data.id,
+            role: 'ADMIN'
+        }
+    })
+}
+
+)
+
 export const functions = [
     syncUserCreationFn,
     syncUserDeletionFn,
-    syncUserUpdationFn
+    syncUserUpdationFn,
+    syncWorkspaceCreation
 ];
