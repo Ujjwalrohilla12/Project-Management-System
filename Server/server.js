dadc21dac2c6029b2544a5791e37bb1b070f4409
AlishaPraveen070 <alishapraveen2010@gmail.com>
2026-03-13 11:09:42 +0530

    add missing controller and route files

--- Server/server.js
+++ Server/server.js
@@ -6,9 +6,9 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./dist/inngest/index.js";
 import { handleClerkWebhook } from "./dist/routes/clerk-webhook.js";
 import workspaceRouter from './dist/routes/workspaceRouter.js';
-
+import projectRouter from './dist/route/projectRouters.js';
+import taskRouter from './dist/route/taskRouters.js';
 import {protect} from './dist/routes/authMiddleware.js';
-import projectRouter from './route/projectRouters.js';
 
 const app = express();
 
@@ -22,7 +22,8 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Routes
 app.use("/api/workspaces", protect ,workspaceRouter);
-app.use("/api/projects", protect, projectRouter)
+app.use("/api/projects", protect, projectRouter);
+app.use("/api/tasks", protect, taskRouter);
 
 const PORT = process.env.PORT || 5000
 
